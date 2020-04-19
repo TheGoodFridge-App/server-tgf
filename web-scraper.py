e95f30be9256774da2ded5230dbd6bd1d6dc669b
Eugene Lo <bleugene12@g.ucla.edu>
2020-04-19 12:32:30 -0700

    added comments

--- web-scraper.py
+++ web-scraper.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
+# list of urls
 urls = [
     'https://www.sustain.ucla.edu/our-initiatives/food-systems/'
 ]
 
+# web scrapes all the urls in the list
 def scrape_urls():
     for url in urls:
         scrape_url(url)
 
+# web scrapes each individual url
 def scrape_url(url):
     try:
         r = requests.get(url)
@@ -17,6 +20,7 @@ def scrape_url(url):
 
         keywords = soup.get_text()
         keywords = keywords.split('\n')
+        # Note: this only works for the first url
         filtered_keywords = list(filter(lambda x: len(x) > 0 and x[:2] == '– ', keywords))
         filtered_keywords = list(map(lambda x: x[2:] if len(x) > 2 else x, filtered_keywords))
         for word in filtered_keywords:
@@ -24,7 +28,7 @@ def scrape_url(url):
     except requests.exceptions.RequestException as e:
         SystemExit(e)
 
-scrape_urls()
+scrape_url()
 
     
         
