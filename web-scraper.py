cb67e64aa475caa01b92d3144822ac85bb860ec2
shrenik0123 <46769965+shrenik0123@users.noreply.github.com>
2020-05-17 14:11:46 -0400

    web-scraper.py with sentiment analysis code

--- web-scraper.py
+++ web-scraper.py
@@ -8,30 +8,39 @@ from google.cloud.language_v1 import enums
 
 
 
+class Server_Object:
+    def __init__(self, name, score, magnitude):
+        self.name = name
+        self.score = score
+        self.magnitude = magnitude
+
+Server_Object arr = []
+
 #Entity Analysis function
 class Entity:
     def __init__(self, document_to_use):
         self.text_content = document_to_use
-        
-    def analyze_entities(text_content):
+
+    def analyze_entities(self):
         client = language_v1.LanguageServiceClient()
-        document = {"content": text_content, "type": type_}
+        document = {"content": self.text_content, "type": type_}
         encoding_type = enums.EncodingType.UTF8
-        response = client.analyze_entities(document, encoding_type=encoding_type)
+        response = client.analyze_entitity_sentiment(document, encoding_type=encoding_type)
         for entity in response.entities:
             print(u"Representative name for the entity: {}".format(entity.name))
-            print(u"Entity type: {}".format(enums.Entity.Type(entity.type).name))
-            print(u"Salience score: {}".format(entity.salience))
-            for metadata_name, metadata_value in entity.metadata.items():
-                print(u"{}: {}".format(metadata_name, metadata_value))
-            for mention in entity.mentions:
-                print(u"Mention text: {}".format(mention.text.content))
-                print(u"Mention type: {}".format(enums.EntityMention.Type(mention.type).name))
+            print(u"Entity sentiment score: {}".format(sentiment.score))
+            print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
+
+
 
 
 # list of urls
 urls = [
     'https://www.sustain.ucla.edu/our-initiatives/food-systems/'
+]
+
+searchs = [
+    'organic products'
 ]
 
 
@@ -52,3 +61,11 @@ class Analysis:
             r = requests.get(a['href'])
             new_soup = BeautifulSoup(r.text, 'html.parser')
             document = new_soup.get_text(separator=" ")
+            return document
+
+
+for search in searches:
+    obj_web_scrape = Analysis(search)
+    doc = obj_web_scrape.run()
+    obj_entity_analysis = Entity(doc)
+    obj_entity_analysis.analyze_entities()
