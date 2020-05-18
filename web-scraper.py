ee840a04e74845b47e6f421f03838518b95b3054
shrenik0123 <46769965+shrenik0123@users.noreply.github.com>
2020-05-17 22:46:02 -0400

    Updated web scraper and sentiment analysis code such that it works now. Only thing left to do is sort out the data

--- web-scraper.py
+++ web-scraper.py
@@ -23,25 +23,26 @@ class Entity:
         self.text_content = document_to_use
 
     def analyze_entities(self):
-        client = language_v1.LanguageServiceClient()
-        document = {"content": self.text_content, "type": type_}
+        type_ = enums.Document.Type.PLAIN_TEXT
+        client = language.LanguageServiceClient()
+        lang = "en"
+        document = {"content": self.text_content, "type": type_, "language": lang}
         encoding_type = enums.EncodingType.UTF8
-        response = client.analyze_entitity_sentiment(document, encoding_type=encoding_type)
+        response = client.analyze_entity_sentiment(document, encoding_type=encoding_type)
         for entity in response.entities:
-            print(u"Representative name for the entity: {}".format(entity.name))
-            print(u"Entity sentiment score: {}".format(sentiment.score))
-            print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
+            sentiment = entity.sentiment
+            if (entity.type=="3" and sentiment.score!=0.0 and entity.name!="producer" and entity.name!="data-entity" and entity.name!="data-entity-type" and entity.name!="producers" and entity.name!="entity-substitution" and entity.name!="data-entity-substitution"):
+                print(u"Representative name for the entity: {}".format(entity.name))
+                print(u"Entity sentiment score: {}".format(sentiment.score))
+                print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
+                print(u"Entity type: {}".format(entity.type))
 
 
 
-
-# list of urls
-urls = [
-    'https://www.sustain.ucla.edu/our-initiatives/food-systems/'
-]
 
 searches = [
-    'organic products'
+    'ethical levels of milk brands'
+    'most ethical milk brands'
 ]
 
 # desktop user-agent
@@ -49,33 +50,41 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100
 # mobile user-agent
 MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 headers = {"user-agent": USER_AGENT}
+text = ""
 
 class Analysis:
     #initializes an object with the correct url
     def __init__(self, object_to_search):
         self.object_to_search = object_to_search
         #self.url = 'https://www.google.com/search?q={0}&source=lnms&tbm=nws'.format(self.object_to_search)
-        object_to_search = object_to_search.replace(' ', '+')
+        query = object_to_search.replace(' ', '+')
         self.url = f"https://google.com/search?q={query}"
-        self.sentiment = 0
 
     def run(self):
-resp = requests.get(self.url, headers=headers)
-i = 0
-if resp.status_code == 200:
-    soup = BeautifulSoup(resp.content, "html.parser")
-    results = []
-    for g in soup.find_all('div', class_='r'):
-        anchors = g.find_all('a')
-        if anchors:
-            link = anchors[0]['href']
-            response = requests.get(link, headers=headers)
-            if response.status_code == 200:
-                    soup_1 = BeautifulSoup(response.content, "html.parser")
-                    text += soup_1.find_all('p')
-                    i += 1
-                    if (i == 10)
-                        return text
+        resp = requests.get(self.url, headers=headers)
+        i = 0
+        text = ""
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.content, "html.parser")
+            results = []
+            for g in soup.find_all('div', class_='r'):
+                anchors = g.find_all('a')
+                if anchors:
+                    link = anchors[0]['href']
+                    if link is not None:
+                        response = requests.get(link, headers=headers)
+                        if response.status_code == 200:
+                            try:
+                                soup_1 = BeautifulSoup(response.content, "html.parser")
+                                text += str(soup_1.find_all('p'))
+                                i += 1
+                                if (i == 20):
+                                    break
+                                    #print(text)
+                                    #return text
+                            except:
+                                pass
+        return(text)
 
 
 for search in searches:
