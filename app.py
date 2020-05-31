44bfc0edecc994db97d4c55c1d771e4a908e9522
Param Shah <paramshah09@gmail.com>
2020-05-31 22:54:33 +0530

    environ.get error handling

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ auth = firebase.auth()
 cred = {
     u"type": "service_account",
     u"project_id": "thegoodfridge-74422",
-    u"private_key_id": environ['PRIVATE_KEY_ID'],
-    u"private_key": environ['PRIVATE_KEY'].replace('\\n', '\n'),
-    u"client_email": environ['CLIENT_EMAIL'],
-    u"client_id": environ['CLIENT_ID'],
+    u"private_key_id": environ.get('PRIVATE_KEY_ID', ''),
+    u"private_key": environ.get('PRIVATE_KEY', '').replace('\\n', '\n'),
+    u"client_email": environ.get('CLIENT_EMAIL', ''),
+    u"client_id": environ.get('CLIENT_ID', ''),
     u"auth_uri": "https://accounts.google.com/o/oauth2/auth",
     u"token_uri": "https://oauth2.googleapis.com/token",
     u"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    u"client_x509_cert_url": environ['CLIENT_CERT_URL']
+    u"client_x509_cert_url": environ.get('CLIENT_CERT_URL', '')
 }
 
 cred = credentials.Certificate(cred)
