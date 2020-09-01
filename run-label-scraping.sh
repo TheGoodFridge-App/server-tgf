08f3610a7fa39bb3fb433f440d75371b012366a6
shrenik0123 <46769965+shrenik0123@users.noreply.github.com>
2020-09-02 03:11:30 +0530

    added comment explaining how to run run-label-scraping.sh

--- run-label-scraping.sh
+++ run-label-scraping.sh
@@ -1,1 +1,2 @@
+# run in the form: python3 label-scraping.py [company name with words separated by spaces(not case sensitive)] i.e. python3 label-scraping.py Cocoa puffs
 python3 label-scraping.py $@
