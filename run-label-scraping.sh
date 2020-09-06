30508239d80b1114550b1dd9e997c9a52fc6dd0e
shrenik0123 <46769965+shrenik0123@users.noreply.github.com>
2020-09-07 00:04:36 +0530

    Added shell script to redirect output to labels.txt

--- run-label-scraping.sh
+++ run-label-scraping.sh
@@ -1,2 +1,3 @@
 # run in the form: python3 label-scraping.py [company name with words separated by spaces(not case sensitive)] i.e. python3 label-scraping.py Cocoa puffs
-python3 label-scraping.py $@
+rm -f labels.txt
+python3 label-scraping.py $@ > labels.txt
