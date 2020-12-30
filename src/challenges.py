e5ce3ac435777421ac23bbf3098de028dd784d97
Eugene Lo <bleugene12@g.ucla.edu>
2020-12-30 13:39:37 -0800

    added debugging statements

--- src/challenges.py
+++ src/challenges.py
@@ -9,6 +9,7 @@ import subprocess
 
 challenges = Blueprint("challenges", __name__)
 
+
 @challenges.route('/update', methods=['PUT', 'POST'])
 def update_user_challenges():
     email = request.args.get('email')
@@ -20,27 +21,31 @@ def update_user_challenges():
     brand = str(request.args.get('brand'))
 
     labels = []
-    ref = db.collection('products_associated_with_labels').where(brand[0], u'array_contains', brand)
+    ref = db.collection('products_associated_with_labels').where(
+        brand[0], u'array_contains', brand)
 
     docs = ref.stream()
     for doc in docs:
         labels.append(doc.id)
-
-    #get challenges related to the labels
+    print(f'labels: {labels}')
+    # get challenges related to the labels
     response = requests.get('https://the-good-fridge.herokuapp.com/challenges/from_labels',
-        params={'labels[]': labels, 'secret[]': environ.get('APP_SECRET')},
-    )
+                            params={'labels[]': labels,
+                                    'secret[]': environ.get('APP_SECRET')},
+                            )
     challenges = list(json.loads(response.content).values())
     if len(challenges) > 0:
         challenges = challenges[0]
-
-    #get the user's data to find overlapping challenges and update if any
-    ref = db.collection('users').document(str(email)).collection('challenges').document('challenges')
+    print(f'challenges: {challenges}')
+    # get the user's data to find overlapping challenges and update if any
+    ref = db.collection('users').document(str(email)).collection(
+        'challenges').document('challenges')
     user_data = ref.get().to_dict()
     user_challenges = list(user_data["challenges"].keys())
-    
-    #get overlapping challenges so we know what to update
-    overlapping_challenges = [challenge for challenge in challenges if challenge in user_challenges]
+    print(f'user challenges: {user_challenges}')
+    # get overlapping challenges so we know what to update
+    overlapping_challenges = [
+        challenge for challenge in challenges if challenge in user_challenges]
 
     if not overlapping_challenges:
         return user_data, 200
@@ -49,36 +54,38 @@ def update_user_challenges():
     data = ref.get().to_dict()
     challenges_info = {}
 
-    #combine everthing into a single top level challenge to values map
+    # combine everthing into a single top level challenge to values map
     for key in data.keys():
         challenges_info.update(data[key])
 
     leveled_up_challenges = []
     completed_challenges = []
-
-    #do the actual updating
+    print(f'overlapping challenges before: {overlapping_challenges}')
+    # do the actual updating
     for challenge in overlapping_challenges:
         levels_info = challenges_info[challenge]
         user_data["challenges"][challenge]["current"] += 1
 
-        #level up the challenge
+        # level up the challenge
         if user_data["challenges"][challenge]["current"] in levels_info:
-            #if level < total levels then just update the level
+            # if level < total levels then just update the level
             if user_data["challenges"][challenge]["level"] != len(levels_info):
                 user_data["challenges"][challenge]["level"] += 1
                 leveled_up_challenges.append(challenge)
 
-            #else remove the challenge because it has been completed
+            # else remove the challenge because it has been completed
             else:
                 del user_data["challenges"][challenge]
                 completed_challenges.append(challenge)
                 user_data["history"].append(challenge)
-
-    #update firestore with updated info
-    ref = db.collection('users').document(str(email)).collection('challenges').document('challenges')
+    print(f'overlapping challenges after: {overlapping_challenges}')
+    # update firestore with updated info
+    ref = db.collection('users').document(str(email)).collection(
+        'challenges').document('challenges')
     ref.update(user_data)
 
     return {"leveled_up": leveled_up_challenges, "completed": completed_challenges}, 200
+
 
 @challenges.route('/get', methods=['GET'])
 def get_user_challenges():
@@ -89,7 +96,8 @@ def get_user_challenges():
         return "Sorry you are not authorized to perform this action", 400
 
     try:
-        ref = db.collection(u'users').document(str(email)).collection('challenges').document('challenges')
+        ref = db.collection(u'users').document(str(email)).collection(
+            'challenges').document('challenges')
         data = ref.get().to_dict()
 
         challenges = data['challenges']
@@ -127,12 +135,14 @@ def get_user_challenges():
         ret = 'Failed with error: ' + str(e)
         return ret, 400
 
+
 def add_challenges(email, challenge_data):
     ref = db.collection(u'users').document(email)
     data = ref.get().to_dict()
 
-    issues = data['animal_issues'] + data['environment_issues'] + data['human_issues']
-    #get challenges for the issues above (could cause problems if not multi-threaded but lets see)
+    issues = data['animal_issues'] + \
+        data['environment_issues'] + data['human_issues']
+    # get challenges for the issues above (could cause problems if not multi-threaded but lets see)
     response = requests.get(
         'https://the-good-fridge.herokuapp.com/challenges/from_issues',
         params={'issues[]': issues, 'secret[]': environ.get('APP_SECRET')},
@@ -141,15 +151,18 @@ def add_challenges(email, challenge_data):
     all_challenges = json.loads(response.content)['challenges']
 
     challenge_arr = list(challenge_data['challenges'].keys())
-    ongoing_and_completed_challenges = challenge_arr + challenge_data['history']
+    ongoing_and_completed_challenges = challenge_arr + \
+        challenge_data['history']
     new_challenges_needed = 3 - len(challenge_arr)
 
-    undone_challenges = [challenge for challenge in all_challenges if challenge not in ongoing_and_completed_challenges]
+    undone_challenges = [
+        challenge for challenge in all_challenges if challenge not in ongoing_and_completed_challenges]
     challenges = dict(challenge_data['challenges'])
 
     for i in range(new_challenges_needed):
-        new_challenge = undone_challenges[random.randint(0, len(undone_challenges))]
-        
+        new_challenge = undone_challenges[random.randint(
+            0, len(undone_challenges))]
+
         challenges[new_challenge] = {
             u'current': 0,
             u'level': 1
@@ -160,14 +173,16 @@ def add_challenges(email, challenge_data):
 
         del undone_challenges[undone_challenges.index(new_challenge)]
 
-    ref = db.collection(u'users').document(email).collection('challenges').document('challenges')
+    ref = db.collection(u'users').document(email).collection(
+        'challenges').document('challenges')
     ref.set({
         'challenges': challenges,
         'history': challenge_data['history']
     })
 
     return challenges
-    
+
+
 @challenges.route('/completed', methods=['GET'])
 def get_completed_challenges():
     email = request.args.get('email')
@@ -177,7 +192,8 @@ def get_completed_challenges():
         return "Sorry you are not authorized to perform this action", 400
 
     try:
-        ref = db.collection(u'users').document(str(email)).collection('challenges').document('challenges')
+        ref = db.collection(u'users').document(str(email)).collection(
+            'challenges').document('challenges')
         data = ref.get().to_dict()
 
         history = data['history']
@@ -188,6 +204,7 @@ def get_completed_challenges():
         ret = 'Failed with error: ' + str(e)
         return ret, 400
 
+
 @challenges.route('/from_issues')
 def get_challenges_from_issues():
     issues = request.args.getlist('issues[]')
@@ -195,7 +212,7 @@ def get_challenges_from_issues():
 
     if secret != environ.get('APP_SECRET'):
         return 'Sorry you are not authorized to perform this action', 400
-        
+
     try:
         ref = db.collection(u'relationships')
         docs = ref.stream()
@@ -234,7 +251,7 @@ def get_challenges_from_labels():
 
     if secret != environ.get('APP_SECRET'):
         return 'Sorry you are not authorized to perform this action', 400
-        
+
     try:
         ref = db.collection(u'relationships')
         docs = ref.stream()
@@ -271,7 +288,7 @@ def get_descriptions():
 
     if secret != environ.get('APP_SECRET'):
         return 'Sorry you are not authorized to perform this action', 400
-        
+
     try:
         ref = db.collection(u'relationships')
         docs = ref.stream()
